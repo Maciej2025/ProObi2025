77e1490f5742a063dcea63bffbd769b4b1b20b53
Maciej2025 <dowlaszewiczmaciej@wp.pl>
2025-03-04 17:01:24 +0100

    test pointa

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         Point p1 = new Point();
+        p1.x = 500;
+        p1.y = 600;
+        p1.translate(400,200);
+        Point p2 = p1.translated(-400,-200);
+        System.out.println(p1.toSvg());
+
 
 
     }
--- src/Point.java
+++ src/Point.java
@@ -1,8 +1,30 @@
 public class Point {
-
     public double x;
     public double y;
 
+    public String toString(){
+        return "x=" + x + " y=" + y;
+    }
+    public String toSvg(){
+        return "<svg height=\"100\" width=\"100\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+                "  <circle r=\"45\" cx=\"" + x +"\" cy=\"" + y+ "\" fill=\"purple\" />\n" + "</svg>";
+    }
 
+    public void translate(double dx, double dy){
+        x += dx;
+        y += dy;
+
+
+
+
+    }
+    public Point translated(double dx, double dy){
+
+        Point trP = new Point();
+        trP.x = x + dx;
+        trP.y = y + dy;
+
+        return trP;
+    }
 
 }
