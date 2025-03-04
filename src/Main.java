e5736fa03e595e25d4f4ffb695bcda446323915b
Maciej2025 <dowlaszewiczmaciej@wp.pl>
2025-03-04 17:16:49 +0100

    test pointa

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         Point p1 = new Point();
-        p1.x = 500;
-        p1.y = 600;
-        p1.translate(400,200);
-        Point p2 = p1.translated(-400,-200);
+        p1.setX(5);
+        p1.setY(6);
+
         System.out.println(p1.toSvg());
 
 
--- src/Point.java
+++ src/Point.java
@@ -1,30 +1,33 @@
 public class Point {
-    public double x;
-    public double y;
+    private double x;
+    private double y;
 
     public String toString(){
         return "x=" + x + " y=" + y;
     }
     public String toSvg(){
         return "<svg height=\"100\" width=\"100\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
-                "  <circle r=\"45\" cx=\"" + x +"\" cy=\"" + y+ "\" fill=\"purple\" />\n" + "</svg>";
+                "  <circle r=\"45\" cx=\"" + x +"\" cy=\"" + y+ "\" fill=\"red\" />\n" +
+                "</svg>";
     }
 
-    public void translate(double dx, double dy){
-        x += dx;
-        y += dy;
 
 
 
 
-    }
-    public Point translated(double dx, double dy){
-
-        Point trP = new Point();
-        trP.x = x + dx;
-        trP.y = y + dy;
-
-        return trP;
+    public double getX() {
+        return x;
     }
 
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
